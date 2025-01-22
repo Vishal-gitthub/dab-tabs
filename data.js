95745393e3024fab745a1be4bf5bb7ff42fd6249
Your Name <you@example.com>
2025-01-22 12:14:28 +0530

    second push

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-unrealeditorexe-360-20250121---19194426",
+      "name": "UnrealEditor.exe 360 2025.01.21 - 19.19.44.26",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.911185272310405,
+          "pitch": -0.1093925709028607,
+          "rotation": 6.283185307179586,
+          "target": "1-u2-20250121---19320400"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-u2-20250121---19320400",
+      "name": "U2 2025.01.21 - 19.32.04.00",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
